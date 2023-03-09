6d59cabe5c5691877a3b3d064578093db4975441
Felix Engel <Felix.Engel@uniklinik-freiburg.de>
2023-03-09 18:10:09 +0100

    Revert to Read the Docs theme and add configurations.

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,8 @@ release = '0.2'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -74,13 +76,16 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#
-# html_theme_options = {}
+
+html_theme_options = {
+	'navigation_depth': 1,
+	'includehidden': True,
+	'titles_only': True}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
